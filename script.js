98e6cb3520e44a88a72a1a4f47afaced40d39fa2
tejas <sadadetej.27@gmail.com>
2023-08-17 07:02:59 +0530

    some changes done

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const header = document.querySelector("header")
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky",window.scrolly>120);
 })
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    // 55min/
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+}
+
+    
